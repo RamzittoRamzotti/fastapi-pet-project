63a35e5e19ea7e3f25fb19e7328a956b0bac132b
RamzittoRamzotti <gilmanovramazan7@gmail.com>
2024-06-04 21:17:06 +0300

    Fix dockerfiles

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +6,6 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+if '__name__' == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=5000)
